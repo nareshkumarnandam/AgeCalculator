3fe2e2c5c26cc67056d4dcecf107fd68e85a8c92
naresh <nareshcena666@gmail.com>
2024-01-07 17:52:02 +0530

    Age calculator made using HTML, CSS & JAVASCRIPT

---
+++ ageCalculator.js
@@ -0,0 +1,31 @@
+const btn = document.getElementById("button");
+const birth_day = document.getElementById("birthday");
+const resultAge = document.getElementById("result");
+
+function calculateAge() {
+  const birthdayValue = birth_day.value;
+  if (birthdayValue === "") {
+    alert("Please enter your birthday");
+  } else {
+    const age = getAge(birthdayValue);
+    resultAge.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
+  }
+}
+
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+btn.addEventListener("click", calculateAge);
